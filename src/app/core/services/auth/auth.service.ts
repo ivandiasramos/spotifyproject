e80b6722a0405c97c3c82830d0ab96c32ec4cb0c
Ivan Ramos <ivanramos@MacBook-Pro-de-Ivan.local>
2019-05-31 22:59:50 -0300

    feat(auth): auth service inicialized

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public spotifyLogin(): Observable<any> {
+    const params = {
+      client_id: '974385e59bef428bb45df3c5836ef0d5',
+      client_secret: '7aad5c9ef159415fb92a18d98afec9a9',
+      grant_type: 'client_credentials',
+    };
+
+    return this.httpClient.post('https://accounts.spotify.com/authorize', params);
+  }
+}
