3372c3e6ee8dba65ab1042ff0c183173156d1c9f
Ivan Ramos <ivanramos@MacBook-Pro-de-Ivan.local>
2019-06-01 13:47:51 -0300

    refactor(artist): artist content component refactored

--- src/app/home/artist-content/artist-content.component.ts
+++ src/app/home/artist-content/artist-content.component.ts
@@ -1,34 +1,18 @@
-import { Subject } from 'rxjs';
-import { Component, OnInit, Input } from '@angular/core';
-import { SpotifyService } from 'src/app/core/services/spotify/spotify.service';
-import { takeUntil } from 'rxjs/operators';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { SpotifyArtist } from 'src/app/shared/interfaces/spotify/artists/spotify.interfaces';
 
 @Component({
   selector: 'app-artist-content',
   templateUrl: './artist-content.component.html',
-  styleUrls: ['./artist-content.component.scss']
+  styleUrls: ['./artist-content.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ArtistContentComponent implements OnInit {
-
-  private unsub$ = new Subject();
+export class ArtistContentComponent {
 
   @Input()
   id: string;
 
-  public spotifyArtist: SpotifyArtist;
-
-  constructor(private spotifyService: SpotifyService) { }
-
-  ngOnInit() {
-    this.spotifyService.getArtist(this.id)
-      .pipe(
-        takeUntil(this.unsub$),
-      ).subscribe((artist: SpotifyArtist) => {
-        this.spotifyArtist = artist;
-
-        console.log('this.spotifyArtist: ', this.spotifyArtist);
-      });
-  }
+  @Input()
+  spotifyArtist: SpotifyArtist;
 
 }
