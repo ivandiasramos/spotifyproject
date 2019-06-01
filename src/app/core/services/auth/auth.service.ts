1ad2d6eff4d56d541a3414ae67bf409272682b77
Ivan Ramos <ivanramos@MacBook-Pro-de-Ivan.local>
2019-06-01 14:47:57 -0300

    spotifyLogin method name moved to getToken

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -9,13 +9,13 @@ export class AuthService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public spotifyLogin(): Observable<any> {
+  public getToken(): Observable<any> {
     const params = {
       client_id: '974385e59bef428bb45df3c5836ef0d5',
       client_secret: '7aad5c9ef159415fb92a18d98afec9a9',
       grant_type: 'client_credentials',
     };
 
-    return this.httpClient.post('https://accounts.spotify.com/authorize', params);
+    return this.httpClient.post('https://accounts.spotify.com/api/token', params);
   }
 }
